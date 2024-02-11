97ccd8c141358a79da53fc466541b3f6206b5e03
candyslug <david.wip.long@gmail.com>
2024-02-11 12:23:46 +0800

    rm

--- src/dungeon_presets.c
+++
@@ -1,7 +0,0 @@
-#include "dungeon_presets.h"
-
-// first dungeon of the game
-void run_intro_dungeon(MapData* mapData) {
-    // generate the dungeon layout
-    mapData = generate_map(DUNGEON_PRESET_BASIC);
-}
